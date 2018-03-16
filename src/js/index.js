563fd6c3fc24d96a5d8c932cc6ef569cadf10b70
marfuzzi <mariavershina@gmail.com>
2018-03-16 23:21:23 +0300

    Вывод веб-камеры на экран

---
+++ src/js/index.js
@@ -0,0 +1,56 @@
+(function () {
+    navigator.getUserMedia = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+    if (!navigator.getUserMedia) {
+        return false;
+    }
+    const video = document.querySelector('.camera__video');
+
+    const canvasInvert = document.querySelector('.canvas__invert');
+    const contextInvert = canvasInvert.getContext('2d');
+    const bgCanvasInvert = document.createElement('canvas');
+    const bgContextInvert = bgCanvasInvert.getContext('2d');
+
+    let width = 0;
+    let height = 0;
+    let loopVideo;
+    let track;
+
+    getWebcam();
+
+    video.addEventListener('loadedmetadata', captureFrame);
+
+    function captureFrame() {
+        width = canvasInvert.width = video.videoWidth;
+        height = canvasInvert.height = video.videoHeight;
+
+        centerX = width / 2;
+        centerY = height / 2;
+
+        bgCanvasInvert.width = width;
+        bgCanvasInvert.height = height;
+
+        startVideo();
+    }
+
+    function startVideo() {
+        loopVideo = loopVideo || requestAnimationFrame(playVideo);
+    }
+
+    function playVideo() {
+        loopVideo = requestAnimationFrame(playVideo);
+    }
+
+    function getWebcam() {
+        navigator.getUserMedia({video: true, audio: false}, function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            track = stream.getTracks()[0];
+        }, function (e) {
+            console.error('Error!', e);
+        });
+    }
+
+})();
